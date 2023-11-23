230eb117c10dcd49187aef80713fdcd05a559cad
Rubén Velasco Zambrano <146188826+RubenVZ03@users.noreply.github.com>
2023-11-23 01:32:07 +0100

    Libro.java
    Código Libro.java actualizado al dia 23/11/2023

--- Libro.java
+++ Libro.java
@@ -1,16 +1,20 @@
 package biblioteca;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-public class Libro implements Serializable {
+public class Libro implements Serializable {	
 	private int id;
 	private String titulo;
 	private String autor;
 	private int anioPublicacion;
 	private String genero;
 	
-	
-	
+	public Libro() {
+		
+	}
 	
 	public Libro(int id, String titulo, String autor, int anioPublicacion, String genero) {
 		super();
@@ -21,80 +25,67 @@ public class Libro implements Serializable {
 		this.genero = genero;
 	}
 
-
-
-
+	public static int obtenerUltimoId(List<Libro> libros) {
+		if (libros == null || libros.isEmpty()) {
+			System.out.println("Error: No hay ningun libro");
+			return 0;
+		} else {
+			int ultimoId = Integer.MIN_VALUE;
+			for (Libro libro : libros) {
+				if (libro.getId() > ultimoId) {
+                    ultimoId = libro.getId();
+                }
+			}
+			return ultimoId;
+		}
+	}
+	
+	
 	public int getId() {
 		return id;
 	}
-
-
-
 
 	public void setId(int id) {
 		this.id = id;
 	}
 
-
-
-
 	public String getTitulo() {
 		return titulo;
 	}
-
-
-
 
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
 
-
-
-
 	public String getAutor() {
 		return autor;
 	}
-
-
-
 
 	public void setAutor(String autor) {
 		this.autor = autor;
 	}
 
-
-
-
 	public int getAnioPublicacion() {
 		return anioPublicacion;
 	}
-
-
-
 
 	public void setAnioPublicacion(int anioPublicacion) {
 		this.anioPublicacion = anioPublicacion;
 	}
 
-
-
-
 	public String getGenero() {
 		return genero;
 	}
 
-
-
-
 	public void setGenero(String genero) {
 		this.genero = genero;
 	}
+
+	@Override
+	public String toString() {
+		return "Libro [id=" + id + ", titulo=" + titulo + ", autor=" + autor + ", anioPublicacion=" + anioPublicacion
+				+ ", genero=" + genero + "]";
+	}
 	
 	
-	
-	
-	
-	
-	// Constructor, getters y setters
 }
