f5ce7a3dd4fb8d60b75924563dd9252e2fdd30ed
Rubén Velasco Zambrano <146188826+RubenVZ03@users.noreply.github.com>
2023-11-23 01:31:34 +0100

    GestorFicheros.java
    Código GestorFicheros.java actualizado al dia 23/11/2023

--- GestorFicheros.java
+++ GestorFicheros.java
@@ -1,19 +1,36 @@
 package biblioteca;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class GestorFicheros {
+	
+	private static String filename = "C:\\Users\\Ruben\\Documents\\librosBinario.bin";
+	
+	// Método guardar libros / escribir libros
 	public void guardarLibrosBinario(List<Libro> libros, String filename) {
-		try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filename))) {
-			oos.writeObject(libros);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	    try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filename, true))) {
+	        oos.writeObject(libros);
+	        oos.close();
+	        System.out.println("Libros escritos correctamente en " + filename);
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    }
 	}
 
+
 	public List<Libro> leerLibrosBinario(String filename) {
-// Implementar la lectura de ficheros binarios
+	    ArrayList<Libro> libros = new ArrayList<>();
+	    try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filename))) {
+	        libros = (ArrayList<Libro>) ois.readObject();
+	        ois.close();
+	    } catch (IOException | ClassNotFoundException e) {
+	        // Si hay algún error, devolveremos una lista vacía
+	        // También puede imprimir el stack trace si es necesario
+	    }
+	    return libros;
 	}
-// Métodos para manejar otros tipos de ficheros
+		
+	// Métodos para manejar otros tipos de ficheros
 }
